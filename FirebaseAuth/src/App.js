4badba55e512284b54b9ce29bcde4e6962141733
Amol Chaudhari <amol.chaudhari@figmd.com>
2019-01-09 19:45:02 +0530

    Login form designed with password fields and properties

--- FirebaseAuth/src/App.js
+++ FirebaseAuth/src/App.js
@@ -2,8 +2,21 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
+import firebase from 'firebase';
 
 class App extends Component {
+
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyAZBuPzCdrG_P_nqEo9DA1j1XUzKFVKPPo",
+            authDomain: "authentication-cb49f.firebaseapp.com",
+            databaseURL: "https://authentication-cb49f.firebaseio.com",
+            projectId: "authentication-cb49f",
+            storageBucket: "authentication-cb49f.appspot.com",
+            messagingSenderId: "738381380185"
+        });
+    }
+
     render () {
         return (
             <View>
--- FirebaseAuth/src/components/LoginForm.js
+++ FirebaseAuth/src/components/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, TextInput } from 'react-native';
+import Input from './common/Input';
 import { 
     CustomButton, 
     Card, 
@@ -7,12 +8,28 @@ import {
 } from './common';
 
 class LoginForm extends Component {
+state = {email: '', password: ''};
 
     render() {
         return (
             <Card>
-                <CardSection />
-                <CardSection />
+                <CardSection>
+                    <Input 
+                    placeholder = "user@example.com"
+                    label = "Email"
+                    value = {this.state.email}
+                    onChangeText = {email => this.setState({ email })}
+                     />
+                </CardSection>
+                <CardSection>
+                <Input 
+                    secureTextEntry
+                    placeholder = "Password"
+                    label = "Password"
+                    value = {this.state.password}
+                    onChangeText = {password => this.setState({ password })}
+                     />
+                </CardSection>
                 <CardSection>
                     <CustomButton>
                         Log In
